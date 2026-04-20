de6375572dc3ebe1ec64fb44d07fe079645d5fbb
Camila Fernanda e Silva Lima <camila.fernanda@unifesp.br>
2026-04-20 10:58:19 -0300

    Padrão singleton.

---
+++ camila_lima_hamburgueria/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+// 1. singleton: configuração da loja
+public class  ConfiguracaoLoja
+{
+    private static ConfiguracaoLoja instance;
+
+    public string Status = "Aberta";
+    public double TaxaBase = 3.0;
+
+    private ConfiguracaoLoja() { }
+    public static ConfiguracaoLoja GetInstance()
+    {
+        if (instance == null)
+        {
+            instance = new ConfiguracaoLoja();
+        }
+        return instance;
+    }
+}
